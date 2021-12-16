3bd98548870f97102c46430032c29ca36936a93a
jonathan-hdz-unosquare <jonathan.hernandez@unosquare.com>
2021-12-15 22:54:58 -0600

    control props alt

--- src/02.-Component-Patterns/hooks/useShoppingCart.ts
+++ src/02.-Component-Patterns/hooks/useShoppingCart.ts
@@ -9,29 +9,15 @@ interface IProductInCart extends Product {
   }
 export const useShoppingCart = () => {
     const [shoppingCart, setShoppingCart] = useState<{ [key: string]: IProductInCart }>({});
+    
     const onProductCountChange = ({ count, product }: IonProductChange) => {
-        setShoppingCart(prevProduct => {
-            const productInCart: IProductInCart = prevProduct[product.id] || { count: 0, ...product };
-            if (Math.max(productInCart.count + count, 0) > 0) {
-                productInCart.count += count;
-                return {
-                    ...prevProduct,
-                    [product.id]: productInCart
-                }
-            }
-            const { [product.id]: toDelete, ...rest } = prevProduct;
-            return { ...rest };
-        });
-
-        /*
         setShoppingCart((prevCart) => {
-          if (count === 0) {
-            const { [product.id]: toDelete, ...rest } = prevCart;
-            return rest;
-          }
-          return { ...shoppingCart, [product.id]: { count, ...product } };
-        });
-        */
+            if (count === 0) {
+              const { [product.id]: toDelete, ...rest } = prevCart;
+              return rest;
+            }
+            return { ...shoppingCart, [product.id]: { count, ...product } };
+          });
     };
     return {onProductCountChange,shoppingCart}
 }
